5b9acca5ce3b78210f20745d83573bdb71e7f95c
Thisa Nabil <mythisa@gmail.com>
2026-03-08 23:02:10 +0700

    update 8 march 2026

--- js/navbar.js
+++ js/navbar.js
@@ -14,7 +14,7 @@ if (sidebar && sidebarToggle) {
   });
 }
 
-fetch("components/navbar.html")
+fetch("../components/navbar.html")
   .then((res) => res.text())
   .then((data) => {
     document.getElementById("navbar").innerHTML = data;
